b55aabd4a89be6f1c4e69278e7efc918fae50de8
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2021-08-20 02:01:37 -0700

    create services file

---
+++ my_diary/lib/services/services.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:my_diary/model/user.dart';
+
+class DiaryServices {
+  final CollectionReference userCollectionReference =
+      FirebaseFirestore.instance.collection('users');
+  Future<void> loginUser(String email, String password) async {
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> createUser(
+      String displayName, BuildContext context, String uid) async {
+    MUser user = MUser(
+      avatarUrl: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
+      displayName: displayName,
+      uid: uid,
+      profession: 'Joker',
+    );
+    userCollectionReference.add(user.toMap());
+  }
+
+  Future<void> update(MUser user, String displayName, String avatarUrl,
+      BuildContext context) async {
+    MUser updateUser = MUser(
+      displayName: displayName,
+      avatarUrl: avatarUrl,
+      uid: user.uid,
+    );
+
+    userCollectionReference.doc(user.id).update(updateUser.toMap());
+  }
+}
