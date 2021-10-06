f50ab50c02137db1893cfad9bdd9a94eb592dda3
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2021-10-06 12:55:48 -0700

    add circular progress indicator to login form

--- my_diary/lib/widgets/login_form.dart
+++ my_diary/lib/widgets/login_form.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:my_diary/screens/main_page.dart';
 import 'package:my_diary/widgets/input_decorator.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     Key key,
     GlobalKey<FormState> formKey,
@@ -19,74 +19,88 @@ class LoginForm extends StatelessWidget {
   final GlobalKey<FormState> _globalKey;
 
   @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool isLogin = false;
+  @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _globalKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextFormField(
-                validator: (value) {
-                  return value.isEmpty ? 'Please enter email' : null;
-                },
-                controller: _emailTextController,
-                decoration: buildInputDecoration('email', 'Example@gmail.com'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextFormField(
-                validator: (value) {
-                  return value.isEmpty ? 'Please enter password' : null;
-                },
-                obscureText: true,
-                controller: _passwordTextController,
-                decoration: buildInputDecoration('password', ''),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                padding: EdgeInsets.all(15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                backgroundColor: Colors.green,
-                textStyle: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              onPressed: () {
-                if (_globalKey.currentState.validate()) {
-                  FirebaseAuth.instance
-                      .signInWithEmailAndPassword(
-                    email: _emailTextController.text,
-                    password: _passwordTextController.text,
-                  )
-                      .then(
-                    (value) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            _emailTextController.text = '';
-                            _passwordTextController.text = '';
-                            return MainPage();
-                          },
-                        ),
-                      );
+    return isLogin
+        ? Center(child: LinearProgressIndicator())
+        : Form(
+            key: widget._globalKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      return value.isEmpty ? 'Please enter email' : null;
                     },
-                  );
-                }
-              },
-              child: Text('Sign In'),
+                    controller: widget._emailTextController,
+                    decoration:
+                        buildInputDecoration('email', 'Example@gmail.com'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      return value.isEmpty ? 'Please enter password' : null;
+                    },
+                    obscureText: true,
+                    controller: widget._passwordTextController,
+                    decoration: buildInputDecoration('password', ''),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    backgroundColor: Colors.green,
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: () {
+                    if (widget._globalKey.currentState.validate()) {
+                      setState(() {
+                        isLogin = true;
+                      });
+                      FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                        email: widget._emailTextController.text,
+                        password: widget._passwordTextController.text,
+                      )
+                          .then(
+                        (value) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                widget._emailTextController.text = '';
+                                widget._passwordTextController.text = '';
+
+                                return MainPage();
+                              },
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: Text('Sign In'),
+                ),
+              ],
             ),
-          ],
-        ));
+          );
   }
 }
