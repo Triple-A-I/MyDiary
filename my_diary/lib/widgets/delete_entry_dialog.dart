c06ffe6af47bd9562e81e0e4f88e5e077f3152ef
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2021-10-06 12:58:36 -0700

    create delete diary class with popup dialog

---
+++ my_diary/lib/widgets/delete_entry_dialog.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:my_diary/model/diary.dart';
+
+class DeleteEntryDialog extends StatelessWidget {
+  const DeleteEntryDialog({
+    Key key,
+    @required this.diary,
+  }) : super(key: key);
+
+  final Diary diary;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        'Delete Entery?',
+        style: TextStyle(color: Colors.red),
+      ),
+      content: Text(
+          'Are you sure you want to delete the entry? \n This action can not be reversed.'),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('diaries')
+                .doc(diary.id)
+                .delete()
+                .then((value) {
+              return Navigator.of(context).pop();
+            });
+          },
+          child: Text('Delete'),
+        ),
+      ],
+    );
+  }
+}
